e68dc4ea225dd0ef9a4e99bca20d012d9df38acd
RAKHISIKKA123 <rakhisikka06@gmail.com>
2024-12-02 10:38:52 +0530

    Add files via upload

---
+++ RAK.C
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int a=7,b=3,c=8,addition,multiplication,subtraction,division;
+	int modulus, equivalent,not_equivalent,greater,lesser;
+	int greater_than_and_equal,lesser_than_and_equal;
+}
